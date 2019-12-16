c8844c627a84e6629c7860e4910ee5d71c030c2d
Lucy <apptime.lucy@gmail.com>
2019-12-16 18:40:30 +0700

    custom cell

---
+++ VideoCameraAndGPUImage3/VideoCameraAndGPUImage3/CellCustom.swift
@@ -0,0 +1,20 @@
+//
+//  CellCustom.swift
+//  SimpleVideoFilter
+//
+//  Created by Lucy on 16/12/19.
+//  Copyright © 2019 Red Queen Coder, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CellCustom: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageview: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
